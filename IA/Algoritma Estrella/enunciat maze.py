bfd527d506a12bdc6f6a5770cbd475534cde4089
naiik21 <nicolas.ratia@gmail.com>
2024-10-08 19:49:38 +0200

    A*

---
+++ IA/Algoritma Estrella/enunciat maze.py
@@ -0,0 +1,70 @@
+import pyamaze as maze
+from queue import PriorityQueue
+
+ROWS = 20
+COLS = 20
+
+def distance(cell1, cell2):
+    
+    return abs(cell1[0]-cell2[0])+abs(cell1[1]-cell2[1])
+
+#print(distance((1,1),(1,2)))
+    
+
+def aStar(m:maze):
+    end=(1,1)
+    start=(m.rows, m.cols)
+    forwardPath={}
+    distancia_recorrida={cell:float("inf") for cell in m.grid}
+
+
+    
+    
+    open=PriorityQueue()
+    open.put((0+distance(start,end),start))
+    
+ 
+    
+    while not open.empty():
+        current=open.get()[1]
+        if current == end:
+
+            break
+
+        for direccion in "SNEW":
+            #print(labertinto[a.position])
+            if m.maze_map[current][direccion] == 1:
+                match direccion:
+                    case 'E':
+                        nextPos=(current[0]+1,current[1])
+                    case 'W':
+                        nextPos=(current[0]-1,current[1])
+                    case 'N':
+                        nextPos=(current[0],current[1]-1)
+                    case 'S':
+                        nextPos=(current[0],current[1]+1)
+                recorrido_auxiliar = distancia_recorrida[current] + 1 
+                if distancia_recorrida[nextPos] > recorrido_auxiliar:
+                    distancia_recorrida[nextPos] = recorrido_auxiliar
+                    forwardPath[nextPos] = current
+                    open.put((distancia_recorrida[nextPos]+distance(nextPos, end) , nextPos))
+    return forwardPath
+            
+
+
+                  
+    #print(m.grid)
+    #print(labertinto[a.position]) {N / S / E / W}
+    #print(labertinto[a.position].get(x)) 0 / 1
+    
+    return forwardPath
+
+m=maze.maze(ROWS,COLS)
+m.CreateMaze()
+#pre_Astar = time.time()
+path = aStar(m)
+#post_Astar = time.time()
+#print(post_Astar - pre_Astar)
+a=maze.agent(m,footprints=True)
+m.tracePath({a:path},delay=5)  
+m.run()
